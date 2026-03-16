04009a03d111de52211d18b952ba197ca8da7d42
stevenstank <stevenstank775@gmail.com>
2026-03-16 15:20:26 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const indexRouter = require("./routes/index");
+
+const app = express();
+const PORT = 3000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/", indexRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
